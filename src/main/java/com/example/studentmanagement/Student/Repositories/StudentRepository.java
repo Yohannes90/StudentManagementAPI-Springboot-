8c90e833323ff0dfee23a117d5fcd72271663b30
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-13 23:26:50 +0300

    changes to repository packaging

--- src/main/java/com/example/studentmanagement/Student/Repositories/StudentRepository.java
+++ src/main/java/com/example/studentmanagement/Student/Repositories/StudentRepository.java
@@ -1,9 +1,8 @@
-package com.example.studentmanagement.Student;
+package com.example.studentmanagement.Student.Repositories;
 
+import com.example.studentmanagement.Student.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
