80f431d5d2826a42f400ad65e3043fe6e7856b38
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-15 19:19:41 +0300

    course controller

---
+++ src/main/java/com/example/studentmanagement/Student/Controllers/CourseController.java
@@ -0,0 +1,28 @@
+package com.example.studentmanagement.Student.Controllers;
+
+import com.example.studentmanagement.Student.Entity.Course;
+import com.example.studentmanagement.Student.Services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/courses")
+public class CourseController {
+
+    private CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping()
+    public List<Course> getCourse() {
+        return courseService.getCourses();
+    }
+
+}
